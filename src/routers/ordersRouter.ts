d6c7e514b083755ea2f3426508ffe2db213c0f88
Luiz Pereira <lfhbpereira@gmail.com>
2023-01-16 01:13:32 -0300

    Remove unnecessary files

--- src/routers/ordersRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import * as ordersController from '../controllers/ordersController';
-
-const router = express.Router();
-
-router.get('/', ordersController.default);
-
-export default router;
