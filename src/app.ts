b5e816b02127d9af978d36da4d9335ab5d31d494
Luiz Pereira <lfhbpereira@gmail.com>
2023-01-16 15:13:57 -0300

    Add requirement 6 done

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,15 @@ import productsController from './controllers/productsController';
 import usersController from './controllers/usersController';
 import ordersController from './controllers/ordersController';
 import validateLogin from './middlewares/validateLogin';
+import validateProductName from './middlewares/validateProductName';
+import validateProductAmount from './middlewares/validateProductAmount';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/products', productsController.getAllProducts);
-app.post('/products', productsController.createProduct);
+app.post('/products', validateProductName, validateProductAmount, productsController.createProduct);
 app.post('/users', usersController.createUser);
 app.get('/orders', ordersController.getAllOrders);
 app.post('/login', validateLogin, usersController.userLogin);
